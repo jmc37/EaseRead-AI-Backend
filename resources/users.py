3632c24afb0f0fcc0e8519655ac3786c9e608634
Joseph Chan <josephchan36@gmail.com>
2023-11-18 23:43:03 -0800

    updated path

--- resources/users.py
+++ resources/users.py
@@ -2,7 +2,7 @@ from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256
 from db import db
-from models import user
+from models import UserModel
 from schemas import UserSchema
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt
 from blocklist import BLOCKLIST
