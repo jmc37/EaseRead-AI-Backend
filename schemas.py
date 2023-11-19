2c94ce374c9967c8d3ddedf5b5d23a5ace65e116
Joseph Chan <josephchan36@gmail.com>
2023-11-19 13:06:16 -0800

    updated schema

--- schemas.py
+++ schemas.py
@@ -4,4 +4,4 @@ class UserSchema(Schema):
     id = fields.Int(dump_only=True)
     username = fields.Str(required=True)
     password = fields.Str(required=True, load_only=True) #Doesn't return password
-    admin = fields.Bool(load_only=True)
+    admin = fields.Bool()
