23b2b3d87885a79df146a8225e6cf7fda373a48d
Joseph Chan <josephchan36@gmail.com>
2023-11-25 11:43:41 -0800

    modified cookie

--- resources/users.py
+++ resources/users.py
@@ -82,7 +82,8 @@ class UserLogin(MethodView):
             max_age=1800,  # Set the max_age to 1800 seconds (30 minutes)
             expires=expiration_time.strftime("%a, %d %b %Y %H:%M:%S GMT"),  # Format expiration time as RFC 1123 string
             httponly=True,
-            secure=True
+            secure=True,
+            samesite='None',  # Add SameSite attribute
         )
             return response
         abort(401, message="Invalid credentials.")
