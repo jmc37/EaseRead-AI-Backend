7cd8778ac2b7ac198f790bedd6ed9056d81d2f23
Joseph Chan <josephchan36@gmail.com>
2023-11-25 18:15:31 -0800

    updated

--- resources/users.py
+++ resources/users.py
@@ -82,7 +82,7 @@ class UserLogin(MethodView):
             max_age=1800,  # Set the max_age to 1800 seconds (30 minutes)
             expires=expiration_time.strftime("%a, %d %b %Y %H:%M:%S GMT"),  # Format expiration time as RFC 1123 string
             httponly=True,
-            secure=True,
+            secure=False,
             samesite='None',  # Add SameSite attribute
         )
             print(response)
