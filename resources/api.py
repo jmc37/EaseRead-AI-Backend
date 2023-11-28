7dfd2dc39ab61ee9f54a86b1651c0ed0d702524a
B-Rabbit7 <snouri3@my.bcit.ca>
2023-11-28 11:51:31 -0800

    updated api endpoint

--- resources/api.py
+++ resources/api.py
@@ -4,19 +4,22 @@ from flask import jsonify
 from flask_smorest import Blueprint, abort
 from db import db
 from models import UserModel, RequestModel
-from flask_jwt_extended import jwt_required, get_jwt
+from flask_jwt_extended import jwt_required, get_jwt,decode_token
 from flask_cors import cross_origin
+from flask import request
 
 API_VERSION = "/API/v1"
 blp = Blueprint("API", "api", description="Operations on api count")
 
 @blp.route(f"{API_VERSION}/allapi")
 class AllAPIRoutes(MethodView):
-    @jwt_required()
     @blp.response(200, description="Get all API routes")
     def get(self):
-        jwt = get_jwt()
-        is_admin = jwt.get("admin", False)
+        # Access the access_token cookie
+        access_token_cookie = request.cookies.get('access_token')
+        # Decode the token using the secret key
+        decoded_token = decode_token(access_token_cookie)
+        is_admin = decoded_token.get("admin", False)
 
         if not is_admin:
             abort(401, message="Admin privilege required")
