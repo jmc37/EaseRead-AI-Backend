66acbe3e85bc93c9a00292a92dbb1fd09652e438
Joseph Chan <josephchan36@gmail.com>
2023-11-27 18:01:51 -0800

    updated feature count

--- resources/api.py
+++ resources/api.py
@@ -1,15 +1,11 @@
 import os
 from flask.views import MethodView
-from flask import jsonify, make_response
+from flask import jsonify
 from flask_smorest import Blueprint, abort
 from db import db
 from models import UserModel, RequestModel
-from schemas import UserSchema, UserRegisterSchema
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt
-from datetime import datetime, timedelta
-from sqlalchemy import or_
+from flask_jwt_extended import jwt_required, get_jwt
 from flask_cors import cross_origin
-from flask import request
 
 API_VERSION = "/API/v1"
 blp = Blueprint("API", "api", description="Operations on api count")
@@ -19,38 +15,38 @@ class UsersList(MethodView):
     @jwt_required()
     @blp.response(200, UserSchema(many=True))
     def get(self):
-        route = RequestModel.query.filter_by(method='GET', endpoint=f'{API_VERSION}/users').first()
+        # Your existing code for user API data retrieval
         jwt = get_jwt()
         users = UserModel.query.all()
         return users
-    
+
 @blp.route(f"{API_VERSION}/allapi")
 class AllAPIRoutes(MethodView):
     @jwt_required()
     @blp.response(200, description="Get all API routes")
     def get(self):
-        # Query all unique API routes recorded in the RequestModel
         jwt = get_jwt()
         is_admin = jwt.get("is_admin", False)
-        api_routes = (
-                    db.session.query(RequestModel)
-                    .distinct(RequestModel.endpoint)
-                    .all()
-                )
 
-        # Convert the result to a list of dictionaries
+        if not is_admin:
+            abort(401, message="Admin privilege required")
+
+        # Query all unique API routes recorded in the RequestModel
+        api_routes = (
+            db.session.query(RequestModel)
+            .distinct(RequestModel.endpoint)
+            .all()
+        )
+
+        # Construct a response object
         api_routes_data = [
             {
                 "id": route.id,
                 "method": route.method,
                 "endpoint": route.endpoint,
-                "requests": route.requests
+                "requests": route.requests,
             }
             for route in api_routes
         ]
-        if not is_admin:
-            abort(401, message="Admin privilege required")
-        # Extract endpoints from the result
 
-
-        return jsonify(api_routes)
+        return jsonify(api_routes_data)
